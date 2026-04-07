1256147d4c6694abd22a46b7a31c97b762c3131c
khaledayman20 <khaled.555.ayman@hotmail.com>
2026-04-07 23:01:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
-menu = {
-    "Burger": 50,
-    "Pizza": 80,
-    "Cola": 20
-}
-
-orders = []
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    total = 0
-
-    if request.method == "POST":
-        item = request.form["item"]
-        qty = int(request.form["qty"])
-        price = menu[item] * qty
-        orders.append((item, qty, price))
-
-    for o in orders:
-        total += o[2]
-
-    tax = total * 0.14
-    final = total + tax
-
-    return render_template("index.html", menu=menu, orders=orders, total=total, tax=tax, final=final)
+    return "Hello from Railway 🚀"
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
