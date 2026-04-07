56226f9790419a8b4b032670a0cfee50e575c687
khaledayman20 <khaled.555.ayman@hotmail.com>
2026-04-07 22:25:06 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+menu = {
+    "Burger": 50,
+    "Pizza": 80,
+    "Cola": 20
+}
+
+orders = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    total = 0
+
+    if request.method == "POST":
+        item = request.form["item"]
+        qty = int(request.form["qty"])
+        price = menu[item] * qty
+        orders.append((item, qty, price))
+
+    for o in orders:
+        total += o[2]
+
+    tax = total * 0.14
+    final = total + tax
+
+    return render_template("index.html", menu=menu, orders=orders, total=total, tax=tax, final=final)
+
+app.run()
